beb06d422a2f8177f323e6e7c7e0739b1e48f0f9
IohanSardinha <ioxsardinha@gmail.com>
2020-04-22 21:39:01 +0100

    adding vehicle mooviment

--- code/MyVehicle.js
+++ code/MyVehicle.js
@@ -7,11 +7,28 @@ class MyVehicle extends CGFobject {
 
     constructor(scene) {
         super(scene);
+        this.reset();
         this.pyramid = new MyPyramid(scene,4,1);
+    }
+
+    reset()
+    {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.rotation = 0;
+        this.speed = 0;
+    }
+
+    update()
+    {
+
     }
 
     display() {
         this.scene.pushMatrix();
+        this.scene.translate(this.x,this.y, this.z);
+        this.scene.rotate(this.rotation,0,1,0);
         this.scene.rotate(Math.PI/2,1,0,0);
         this.scene.scale(0.5,1,0.5);
         this.pyramid.display();
