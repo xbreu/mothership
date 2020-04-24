f1b11a319a31622c344c5a82d83b145b4f191a6b
Alexandre Abreu <up201800168@up.pt>
2020-04-24 17:35:50 +0100

    balloon

--- code/MyVehicle.js
+++ code/MyVehicle.js
@@ -9,6 +9,7 @@ class MyVehicle extends CGFobject {
         super(scene);
         this.reset();
         this.pyramid = new MyPyramid(scene, 4, 1);
+        this.balloon = new MySphere(scene, 16, 8);
     }
 
     reset() {
@@ -40,7 +41,7 @@ class MyVehicle extends CGFobject {
         this.scene.rotate(this.rotation, 0, 1, 0);
         this.scene.rotate(Math.PI / 2, 1, 0, 0);
         this.scene.scale(0.5*scale, 1*scale, 0.5*scale);
-        this.pyramid.display();
+        this.balloon.display();
         this.scene.popMatrix();
     }
 
