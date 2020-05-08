f31010243ec5297ac7106e0360d0ada611062cf8
Alexandre Abreu <up201800168@up.pt>
2020-05-08 17:53:43 +0100

    Drawing terrain on universe

--- code/MyScene.js
+++ code/MyScene.js
@@ -30,21 +30,17 @@ class MyScene extends CGFscene {
         this.cylinder = new MyCylinder(this, 10, 0);
         this.cubemap = new MyUnitCubeQuad(this);
         this.vehicle = new MyVehicle(this);
+        this.terrain = new MyTerrain(this);
 
         //Initialize textures
-        this.earthTexture = new CGFappearance(this);
-        this.earthTexture.setAmbient(0.1, 0.1, 0.1, 1);
-        this.earthTexture.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.earthTexture.setSpecular(0.1, 0.1, 0.1, 1);
-        this.earthTexture.setShininess(10.0);
-        this.earthTexture.loadTexture('images/earth.jpg');
-        this.earthTexture.setTextureWrap('REPEAT', 'REPEAT');
+        this.initTextures();
 
         //Objects connected to MyInterface
         this.displayAxis = true;
         this.displayCylinder = false;
         this.displaySphere = false;
         this.displayNormals = false;
+        this.displayTerrain = true;
         this.nightMode = false;
 
         this.selectedMapTexture = 0;
@@ -64,6 +60,24 @@ class MyScene extends CGFscene {
         this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
     }
 
+    initTextures() {
+        this.earthTexture = new CGFappearance(this);
+        this.earthTexture.setAmbient(0.1, 0.1, 0.1, 1);
+        this.earthTexture.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.earthTexture.setSpecular(0.1, 0.1, 0.1, 1);
+        this.earthTexture.setShininess(10.0);
+        this.earthTexture.loadTexture('images/earth.jpg');
+        this.earthTexture.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.terrainTexture = new CGFappearance(this);
+        this.terrainTexture.setAmbient(0.1, 0.1, 0.1, 1);
+        this.terrainTexture.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.terrainTexture.setSpecular(0.1, 0.1, 0.1, 1);
+        this.terrainTexture.setShininess(10.0);
+        this.terrainTexture.loadTexture('images/terrain.jpg');
+        this.terrainTexture.setTextureWrap('REPEAT', 'REPEAT');
+    }
+
     setDefaultAppearance() {
         this.setAmbient(0.2, 0.4, 0.8, 1.0);
         this.setDiffuse(0.2, 0.4, 0.8, 1.0);
@@ -71,8 +85,7 @@ class MyScene extends CGFscene {
         this.setShininess(10.0);
     }
 
-    updateMapTexture()
-    {
+    updateMapTexture() {
         console.log(this.selectedMapTexture);
         this.cubemap.setNightMode(this.selectedMapTexture == 1);
     }
@@ -137,7 +150,7 @@ class MyScene extends CGFscene {
 
         //This sphere does not have defined texture coordinates
 
-        this.vehicle.display(this.scaleFactor);
+        //this.vehicle.display(this.scaleFactor);
 
         if (this.displayCylinder) {
             this.earthTexture.apply();
@@ -150,7 +163,19 @@ class MyScene extends CGFscene {
         if (this.displayNormals)
             this.cylinder.enableNormalViz();
 
-        this.scale(50, 50, 50);
+        this.scale(10, 10, 10);
+        let terrainScale = 50;
+        if (this.displayTerrain) {
+            this.terrainTexture.apply();
+            this.pushMatrix();
+            this.translate(0, -terrainScale / 2 + 0.1, 0);
+            this.rotate(Math.PI / 2, 1, 0, 0);
+            this.rotate(Math.PI, 0, 1, 0);
+            this.scale(terrainScale, terrainScale, 1);
+            this.terrain.display();
+            this.popMatrix();
+        }
+        this.scale(terrainScale, terrainScale, terrainScale);
         this.cubemap.display();
         // ---- END Primitive drawing section
     }
