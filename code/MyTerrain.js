40aa6ca6abe22e082a8a945a240db87c7908732d
a3brx <a3brx@sapo.pt>
2020-05-08 18:00:38 +0100

    Added shader

--- code/MyTerrain.js
+++ code/MyTerrain.js
@@ -1,10 +1,13 @@
 class MyTerrain extends CGFobject {
-    constructor(scene, image="images/terrain.jpg", heightmap="images/heightmap.jpg") {
+    constructor(scene, image = "images/terrain.jpg", heightmap = "images/heightmap.jpg") {
         super(scene);
         this.plane = new MyPlane(scene, 1);
+        this.terrainShader = new CGFshader(this.scene.gl, "shaders/terrain.vert", "shaders/terrain.frag");
     }
 
     display() {
+        this.scene.setActiveShader(this.terrainShader);
         this.plane.display();
+        this.scene.setActiveShader(this.scene.defaultShader);
     }
 }
