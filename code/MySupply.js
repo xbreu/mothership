83e7ba210c56ecf74b234ccb744d561a9a13faf4
IohanSardinha <ioxsardinha@gmail.com>
2020-05-22 12:57:52 +0100

    Supplies - LANDED

--- code/MySupply.js
+++ code/MySupply.js
@@ -9,7 +9,8 @@ class MySupply extends CGFobject {
         super(scene);
         this.reset();
         this.initFaces();
-        this.bottomPosition = 0.2;
+        this.bottomPosition = 0.3;
+        this.diamondRotation = 0;
     }
 
     initFaces() {
@@ -19,6 +20,8 @@ class MySupply extends CGFobject {
         this.back = new MyQuad(this.scene);
         this.right = new MyQuad(this.scene);
         this.left = new MyQuad(this.scene);
+        this.diamond = new MySphere(this.scene,4,1);
+
         this.texture = new CGFappearance(this.scene);
         this.texture.setAmbient(10.0, 10.0, 10.0, 1);
         this.texture.setDiffuse(0.0, 0.0, 0.0, 1);
@@ -26,6 +29,14 @@ class MySupply extends CGFobject {
         this.texture.setShininess(1.0);
         this.texture.loadTexture('images/box.jpg');
         this.texture.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.diamondTexture = new CGFappearance(this.scene);
+        this.diamondTexture.setAmbient(10.0, 10.0, 10.0, 1);
+        this.diamondTexture.setDiffuse(0.0, 0.0, 0.0, 1);
+        this.diamondTexture.setSpecular(10.0, 10.0, 10.0, 1);
+        this.diamondTexture.setShininess(2.0);
+        this.diamondTexture.loadTexture('images/diamond.jpg');
+        this.diamondTexture.setTextureWrap('REPEAT', 'REPEAT');
     }
 
     displayShape(shape, color) {
@@ -49,7 +60,6 @@ class MySupply extends CGFobject {
     }
 
     land() {
-        this.y = this.bottomPosition;
         this.state = SupplyStates.LANDED;
     }
 
@@ -60,20 +70,72 @@ class MySupply extends CGFobject {
             if (this.y <= this.bottomPosition)
                 this.land();
         }
+        if(this.state === SupplyStates.LANDED)
+        {
+            this.diamondRotation += (Math.PI/2)*0.05;
+        }
     }
 
-    display(scale) {
-        this.bottomPosition = 0.2*scale;
-        if (this.state === SupplyStates.INACTIVE)
-            return;
+    bottomPositionFunction(scale)
+    {
+        return 0.141206 + 0.111809*scale + 0.03567839*(scale*scale);
+    }
+
+    displayFalling(scale)
+    {
+        this.bottomPosition = this.bottomPositionFunction(scale);
+
         this.scene.pushMatrix();
         this.scene.translate(this.x, this.y, this.z);
         this.scene.scale(0.4 * scale, 0.4 * scale, 0.4 * scale);
 
         this.scene.pushMatrix();
         this.scene.translate(0, 0.5, 0);
-        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
         this.displayShape(this.top, this.texture);
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, -0.5, 0);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.displayShape(this.bottom, this.texture);
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.5, 0, 0);
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
+        this.displayShape(this.right, this.texture);
+
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0, 0);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.displayShape(this.left, this.texture);
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5);
+        this.scene.rotate(Math.PI, 0, 1, 0);
+        this.displayShape(this.front, this.texture);
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, -0.5);
+        this.displayShape(this.back, this.texture);
+
+        this.scene.popMatrix();
+    }
+
+
+    displayLanded(scale)
+    {
+        this.bottomPosition = this.bottomPositionFunction(scale);
+        this.y = this.bottomPosition;
+        
+        this.scene.pushMatrix();
+        this.scene.translate(this.x, this.y, this.z);
+        this.scene.scale(0.4 * scale, 0.4 * scale, 0.4 * scale);
+
+        this.scene.pushMatrix();
+        this.scene.translate(0,this.y*0.5,0);
+        this.scene.rotate(this.diamondRotation,0,1,0);
+        this.scene.scale(0.2*scale,0.2*scale,0.2*scale);
+        this.displayShape(this.diamond,this.diamondTexture);
 
         this.scene.pushMatrix();
         this.scene.translate(0, -0.5, 0);
@@ -81,24 +143,38 @@ class MySupply extends CGFobject {
         this.displayShape(this.bottom, this.texture);
 
         this.scene.pushMatrix();
-        this.scene.translate(0.5, 0, 0);
-        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.scene.translate(1, -0.5, 0);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
         this.displayShape(this.right, this.texture);
 
         this.scene.pushMatrix();
-        this.scene.translate(-0.5, 0, 0);
-        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
+        this.scene.translate(-1, -0.5, 0);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
         this.displayShape(this.left, this.texture);
 
         this.scene.pushMatrix();
-        this.scene.translate(0, 0, 0.5);
+        this.scene.translate(0, -0.5, 1);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
         this.displayShape(this.front, this.texture);
 
         this.scene.pushMatrix();
-        this.scene.translate(0, 0, -0.5);
-        this.scene.rotate(Math.PI, 0, 1, 0);
+        this.scene.translate(0, -0.5, -1);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
         this.displayShape(this.back, this.texture);
 
         this.scene.popMatrix();
+    }
+
+
+    display(scale) {
+        switch(this.state)
+        {
+            case SupplyStates.FALLING:
+            this.displayFalling(scale);
+            break;
+            case SupplyStates.LANDED:
+            this.displayLanded(scale);
+            break;
+        }   
     }
 }
